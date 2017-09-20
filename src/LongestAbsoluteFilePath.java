02994d2a7b778f3cb23e687d4730008cfd581338
lingjmeng <lingjmeng@ebay.com>
2017-09-19 23:29:05 -0700

    LeetCode 388. Longest Absolute File Path

---
+++ src/LongestAbsoluteFilePath.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+/**
+ * Created by lingjmeng on 9/19/17.
+ */
+public class LongestAbsoluteFilePath {
+    public static int lengthLongestPath(String input) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); // "dummy" length
+        int maxLen = 0;
+        for(String s:input.split("\n")){
+            int lev = s.lastIndexOf("\t")+1; // number of "\t" , "\t" is count as ONE character
+            while(lev+1<stack.size()) stack.pop(); // find parent
+            int len = stack.peek()+s.length()-lev+1; // remove "/t", add"/"
+            stack.push(len);
+            // check if it is file
+            if(s.contains(".")) maxLen = Math.max(maxLen, len-1);
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        System.out.println(lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"));
+    }
+}
