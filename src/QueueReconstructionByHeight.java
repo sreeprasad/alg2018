c42eb1208441a0972842f29036355476d164c532
Lingjie Meng Git <sanguinemeng@gmail.com>
2018-01-10 18:55:10 -0800

    406. Queue Reconstruction by Height

---
+++ src/QueueReconstructionByHeight.java
@@ -0,0 +1,43 @@
+
+/*
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+ */
+
+import helpers.PrintArray;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class QueueReconstructionByHeight {
+    public static int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people, (people0, people1) ->
+                (people0[0] == people1[0] ? people0[1] - people1[1] : people1[0] - people0[0])
+        );
+
+        List<int[]> res = new LinkedList<>();
+        for (int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[people.length][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        int[][] output = reconstructQueue(input);
+        PrintArray.print2dimArray(output);
+    }
+}
