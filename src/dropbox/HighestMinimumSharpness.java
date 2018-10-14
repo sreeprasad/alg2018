9e7ff91c4c3dd5812323f15855974a15f2b2edf2
Lingjie Meng <lingjmeng@ebay.com>
2018-10-14 16:23:45 -0700

    add description to HighestMinimumSharpness

--- src/dropbox/HighestMinimumSharpness.java
+++ src/dropbox/HighestMinimumSharpness.java
@@ -1,14 +1,31 @@
 package dropbox;
 
-import helpers.PrintArray;
-
-// Given a 2-d array of "sharpness" values. Find a path from the leftmost column to the rightmost column which has the highest minimum sharpness.
-// Output the highest minimum sharpness. Each move can only move to the top right, right or bottom right grid.
-// Example: 3*3 matrix
-// 5 7 2
-// 7 5 8
-// 9 1 5
-// The path with highest minimum sharpness is 7-->7-->8, because 7 is the highest minimum value in all the paths.
+/**
+ * Description 1:
+ * You’re given an elevation map for a rectangular area of land. The map is represented by 2-D array of numbers where each cell contains the elevation above sea level of the corresponding area of the map.
+ * You need a path that connects the west edge of the map with the east edge of the map. Starting at the west edge of the map you can only move in single cell steps east, southeast, or northeast.
+ * You need to find how much can the sea level rise before submerging all such paths. Write a function that takes in a 2-D array and returns a single number..留学论坛-一亩-三分地
+ *
+ * 例子：
+ * west         east
+ * ----------------- north
+ * | 1 | 2 | 3 | 9 |
+ * -----------------
+ * | 8 | 6 | 10| 8 |
+ * -----------------
+ * | 9 | 4 | 11| 12|
+ * -----------------  south// sea=6
+ */
+/**
+ * Description 2:
+ * Given a 2-d array of "sharpness" values. Find a path from the leftmost column to the rightmost column which has the highest minimum sharpness.
+ * Output the highest minimum sharpness. Each move can only move to the top right, right or bottom right grid.
+ * Example: 3*3 matrix
+ * 5 7 2
+ * 7 5 8
+ * 9 1 5
+ The path with highest minimum sharpness is 7-->7-->8, because 7 is the highest minimum value in all the paths.
+ */
 public class HighestMinimumSharpness {
     public static void main(String[] args) {
         int[][] image = {{5,7,2}, {7,5,8}, {9,1,5}};
