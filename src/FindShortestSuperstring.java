be8ca03d1847f27b050c92b200bcc3da97c98f84
Lingjie Meng <lingjmeng@ebay.com>
2018-11-19 22:16:14 -0800

    improved solution for FindShortestSuperstring

--- src/FindShortestSuperstring.java
+++ src/FindShortestSuperstring.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 943. Find the Shortest Superstring
@@ -52,10 +49,11 @@ public class FindShortestSuperstring {
 		return shortest;
 	}
 	private void dfs(Map<String, Map<String, Integer>> graph, String path, String prev, Set<String> visited) {
+		if (path.length() > shortest.length()) { // prune
+			return;
+		}
 		if (visited.size() == graph.size()) {
-			if (path.length() < shortest.length()) {
-				shortest = new String(path);
-			}
+			shortest = path;
 			return;
 		}
 		for (String s2 : graph.get(prev).keySet()) {
@@ -75,10 +73,73 @@ public class FindShortestSuperstring {
 		}
 		return 0;
 	}
+
+
+
+
+	// another DFS
+	private int bestLen = Integer.MAX_VALUE;
+	private int[] bestPath;
+	private int[][] overlaps;
+
+	public String shortestSuperstringII(String[] A) {
+		int len = A.length;
+
+		overlaps = new int[len][len];
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < len; j++) {
+				int overlap = overlap(A[i], A[j]);
+				overlaps[i][j] = overlap;
+			}
+		}
+		dfsII(A, 0, 0, 0, new int[len]);
+		StringBuilder result = new StringBuilder().append(A[bestPath[0]]);
+		for (int i = 1; i < bestPath.length; ++i) {
+			int j = bestPath[i - 1];
+			int k = bestPath[i];
+			result.append(A[k].substring(overlaps[j][k]));
+		}
+		return result.toString();
+	}
+	private void dfsII(String[] A, int depth, int used, int curLen, int[] path) {
+		if (curLen > bestLen) { // prune
+			return;
+		}
+		if (depth == A.length) {
+			bestLen = curLen;
+			bestPath = path.clone();
+			return;
+		}
+		for (int i = 0; i < A.length; i++) {
+			if ((used & (1 << i)) > 0) {
+				continue;
+			}
+			path[depth] = i;
+			dfsII(A, depth + 1, (used | (1 << i)),
+					depth == 0 ? A[i].length() : curLen + A[i].length() - overlaps[path[depth - 1]][i], path); // trick part
+		}
+	}
+
+
+
+	// DP (very complex)
+	// https://www.youtube.com/watch?v=u_Wc4jwrp3Q
+//	public String shortestSuperstringIII(String[] A) {
+//
+//	}
+
+
 	public static void main(String[] args) {
 		FindShortestSuperstring fss = new FindShortestSuperstring();
-		System.out.println(fss.shortestSuperstring(new String[]{"catg","ctaagt","gcta","ttca","atgcatc"}).equals("gctaagttcatgcatc")); // "gctaagttcatgcatc"
+		System.out.println(fss.shortestSuperstring(new String[]{"catg","ctaagt","gcta","ttca","atgcatc"}).equals("gctaagttcatgcatc"));
 		// Got TLE for this test case
 		System.out.println(fss.shortestSuperstring(new String[]{"uhozqhxcqmkifljvcie","epuhozqhxcqmkifljvci","ugmqnepuhozqhxcqm","iexdudtvurjkrovrhmpa","rovrhmpaasblgaosw","qmkifljvciexdudtv","zsgtuowskyzphybeugm","uowskyzphybeugmq","qhxcqmkifljvciex"}).equals("zsgtuowskyzphybeugmqnepuhozqhxcqmkifljvciexdudtvurjkrovrhmpaasblgaosw"));
+
+		System.out.println(fss.shortestSuperstringII(new String[]{"catg","ctaagt","gcta","ttca","atgcatc"}).equals("gctaagttcatgcatc"));
+		System.out.println(fss.shortestSuperstringII(new String[]{"uhozqhxcqmkifljvcie","epuhozqhxcqmkifljvci","ugmqnepuhozqhxcqm","iexdudtvurjkrovrhmpa","rovrhmpaasblgaosw","qmkifljvciexdudtv","zsgtuowskyzphybeugm","uowskyzphybeugmq","qhxcqmkifljvciex"}).equals("zsgtuowskyzphybeugmqnepuhozqhxcqmkifljvciexdudtvurjkrovrhmpaasblgaosw"));
+
+//		System.out.println(fss.shortestSuperstringIII(new String[]{"catg","ctaagt","gcta","ttca","atgcatc"}).equals("gctaagttcatgcatc"));
+//		System.out.println(fss.shortestSuperstringIII(new String[]{"uhozqhxcqmkifljvcie","epuhozqhxcqmkifljvci","ugmqnepuhozqhxcqm","iexdudtvurjkrovrhmpa","rovrhmpaasblgaosw","qmkifljvciexdudtv","zsgtuowskyzphybeugm","uowskyzphybeugmq","qhxcqmkifljvciex"}).equals("zsgtuowskyzphybeugmqnepuhozqhxcqmkifljvciexdudtvurjkrovrhmpaasblgaosw"));
+
 	}
 }
